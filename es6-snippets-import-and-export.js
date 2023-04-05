b4a564d629e4da46049d20067a41d00467f68ca2
Richard Hounslow <rhounslow@gmail.com>
2023-04-05 11:15:51 +1000

    updates

--- es6-snippets-import-and-export.js
+++ es6-snippets-import-and-export.js
@@ -16,7 +16,7 @@ import 'module';
 var es5Module = require("./es5Module");
 
 //imports es6 only a prttion of the module
-import { es6Module } from "./es6Module"; 
+import { es6Module } from "./es6Module";
 
 //imports everything as alias from the module
 import * as es6ModuleAlias from "./es6Module";
@@ -29,7 +29,7 @@ import { originalName as alias } from 'module';
 //require package require(''); (rqr)
 require('package');
 
-//require es6 package
+//require es6 package   
 import package from "package";
 import { env } from "process";
 
@@ -44,11 +44,11 @@ export const nameVariable = localVariable;
 
 //exports name function export const log = (parameter) => { console.log(parameter);}; (enf)
 export const functionName = (params) => {
-    
+
 };
 
 //exports default function export default function fileName (parameter){ console.log(parameter);}; (edf)
-export default function fileName (parameter){
+export default function fileName(parameter) {
     console.log(parameter);
 };
 
@@ -57,10 +57,10 @@ export default class Calculator { };
 
 export default class className {
     constructor() {
-        
+
     }
 };
 
-//exports default class by extending a base one export default class Calculator extends BaseClass { }; (ece)
+//exports default class by extending a base one export default class Calculator extends BaseClass { }; 
 export default class Calculator extends BaseClass { };
 
